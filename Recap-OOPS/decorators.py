d8b3f3ed71a0637a39de81dab8864045f1014c3b
poorvaditya behre <aditya.behre@gmail.com>
2023-10-29 17:46:15 +0530

    decorators can also take args and kwargs

--- Recap-OOPS/decorators.py
+++ Recap-OOPS/decorators.py
@@ -1,23 +1,24 @@
 
+# decorators : Generally good practice to pass *args and **kwargs to decorators to be universally accessible. 
 def mydecorator(function):
 
-    def wrapper():
-        print("I am decorating function your function!")
-        function()
+    def wrapper(*args, **kwargs):
+        print(f"I am decorating function your function! {args[0]} ")
+        function(*args, **kwargs)
 
     return wrapper
 
 
 # now we want to decorate this hello world function
 # means you want additonal functionalities 
-def hello_world():
-    print("hello world!")
+# def hello_world():
+#     print("hello world!")
 
 
 # mydecorator(hello_world) --> this returns a wrapper function 
 # --> now to call this fuction myfunction(hello_world)()
 
-mydecorator(hello_world)()
+# mydecorator(hello_world)()
 
 # output: 
 # I am decorating function your function!
@@ -28,8 +29,8 @@ mydecorator(hello_world)()
 # modern way to use decorators : 
 
 @mydecorator
-def HelloWorld():
-    print("Helloooo Modern World!")
+def HelloWorld(name):
+    print(f"Helloooo Modern {name} World!")
 
 
-HelloWorld()
+HelloWorld("Mike")
